a2758db826aabcbb606ac4ec6522207481716d79
CoutCout <72403468+ArthurCouturier@users.noreply.github.com>
2023-01-24 11:51:20 +0100

    sortie quand on clique sur euro en bas par exemple, ca affiche google photo dans le whiteboard, ca chaneg le titre et ça écrit dans la console

--- src/Components/Currency.tsx
+++ src/Components/Currency.tsx
@@ -4,6 +4,7 @@ export default function Currency(props: { currency: string, selectCurrency: (cur
     const { currency, selectCurrency } = props;
 
     const changeCurrency = async () => {
+        console.log("je vais envoyer")
         selectCurrency(currency);
         const requestOptions = {
             method: 'GET',
@@ -12,8 +13,8 @@ export default function Currency(props: { currency: string, selectCurrency: (cur
                 'Access-Control-Allow-Origin': '*'
             }
         };
-        await fetch('http://localhost:3001/', requestOptions)
-            .then(response => console.log(response));
+        await fetch('http://localhost:3001/', requestOptions);
+        console.log("j'ai envoye")
     }
 
     return (
--- src/whiteBoardConnection.js
+++ src/whiteBoardConnection.js
@@ -16,7 +16,7 @@ app.use(function (req, res, next) {
     // Pass to next layer of middleware
     next();
 });
-var cors = require('cors');
+const cors = require('cors');
 // use it before all route definitions
 app.use(cors({origin: 'http://localhost:3000'}));
 app.get('/', (req, res) => {
@@ -33,4 +33,37 @@ const igs = require('ingescape');
 function sendTest() {
     igs.info("Test");
     console.log("igs.info done");
+    igs.outputSetString("sortie1", "COUCOU")
+    igs.outputSetString("sortie3", "JSUIS LA")
+
+    let argsList = [];
+// argsList = igs.serviceArgsAddBool(argsList, this.boolI);
+// argsList = igs.serviceArgsAddInt(argsList, this.integerI);
+    argsList = igs.serviceArgsAddString(argsList, "https://play-lh.googleusercontent.com/ZyWNGIfzUyoajtFcD7NhMksHEZh37f-MkHVGr5Yfefa-IX7yj9SMfI82Z7a2wpdKCA=w480-h960");
+    argsList = igs.serviceArgsAddDouble(argsList, 200);
+    argsList = igs.serviceArgsAddDouble(argsList, 100);
+// argsList = igs.serviceArgsAddData(argsList, this.dataI);
+
+// // image a convertir en base 64 sinon addImageFromUrl
+    igs.serviceCall("Whiteboard", "addImageFromUrl", argsList, "");
 };
+
+
+
+const iopValueTypes = igs.iopValueTypes();
+
+igs.agentSetName("agent1");
+igs.agentSetName("agent2");
+
+
+igs.logSetConsole(true);
+igs.logSetFile(true, null);
+igs.logSetStream(true);
+igs.definitionSetVersion("1.0");
+
+igs.outputCreate("sortie1", iopValueTypes.IGS_STRING_T, "");
+igs.outputCreate("sortie2", iopValueTypes.IGS_STRING_T, "");
+igs.outputCreate("sortie3", iopValueTypes.IGS_STRING_T, "");
+
+//actually start ingescape
+igs.startWithDevice("en0", 5670);
