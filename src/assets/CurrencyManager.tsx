5c17307ebe81336e2167cf20e7edad9f9831a20a
villemontc <cedric.villemont@gmail.com>
2023-01-27 11:05:43 +0100

    getCurrencyNames done & working

--- src/assets/CurrencyManager.tsx
+++ src/assets/CurrencyManager.tsx
@@ -25,28 +25,23 @@ export default class CurrencyManager {
         }
     }
 
-    // public getCurrencyNames(price: number): string[] {
-    //     const currencyNames = [100,20,10,5,2,1];
-    //     const correspondingNames = [];
-    //     let remainingPrice = price;
-    //
-    //     for (const name of currencyNames) {
-    //         const divisor = parseInt(name);
-    //         while (remainingPrice >= divisor) {
-    //             remainingPrice -= divisor;
-    //             correspondingNames.push(name);
-    //         }
-    //     }
-    //     return correspondingNames;
-    // }
-    // const price = 376;
-    // const decomposition = [
-    //     "100",
-    //     "100",
-    //     "cent",
-    //     "cinquante",
-    //     "vingt",
-    //     "cinq",
-    //     "un"
-    // ]
+    public getCurrencyNames(price: number): string[] {
+        const decomposition : string[] = [];
+        const cutting : number[] = [100, 50, 20, 10, 5, 2, 1, 0.5, 0.25, 0.10, 0.05, 0.02, 0.01];
+    
+        for (const c of cutting) {
+            const nb = Math.floor(price / c);
+          
+            for (let i = 0; i < nb; i++) {
+                decomposition.push(c.toString());
+            }
+    
+            price -= nb * c;
+            // used to round typescript imprecision
+            price = Math.round((price + Number.EPSILON) * 100) / 100;
+        }
+    
+        return decomposition;
+    }
+
 };
